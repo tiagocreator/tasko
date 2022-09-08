d4377f373b8e1362d870f67719558387f0bc099d
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-08 02:29:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { GlobalStyles } from './components/styles/GlobalStyles';
+import Header from './components/Header';
+import { ThemeProvider } from 'styled-components';
+import { light } from './themes';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={light}>
+      <>
+        <GlobalStyles />
+        <Header />
+      </>
+    </ThemeProvider>
+  );
 }
 
 export default App;
