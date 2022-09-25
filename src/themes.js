d8e52a90dcb221ce23a3116d57c4f0a402bb4968
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-25 11:14:59 -0300

    Update theme colors

--- src/themes.js
+++ src/themes.js
@@ -1,5 +1,12 @@
 export const light = {
-  primary: '#ff705d',
-  background: '#eaedee',
+  black: '#000',
+  white: '#fff',
+  themeRed: '#ff705d',
+  themeBlue: '#646ff0',
+  secondaryBtn: '#606468',
   text: '#202023',
+  bg1: '#f8f8f8',
+  bg2: '#ecedf6',
+  bg3: '#cccdde',
+  bg4: '#dedfe1',
 };
