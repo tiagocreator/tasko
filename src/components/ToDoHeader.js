1bafb5209afaeb9951901ff1f27c5a1768dc55b6
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-25 11:16:54 -0300

    Create ToDo header component

---
+++ src/components/ToDoHeader.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import { SecondaryButton } from './Button';
+import Select from './Select';
+import ToDoModal from './ToDoModal';
+
+import { ToDoHeaderStyle } from './styles/ToDoHeaderStyles';
+
+const ToDoHeader = () => {
+  const [modalActive, setModalActive] = useState(false);
+
+  const openModal = () => {
+    setModalActive(true);
+  };
+
+  return (
+    <ToDoHeaderStyle>
+      <SecondaryButton onClick={openModal} text="Add Task" />
+      <Select>
+        <option value="all">Todos</option>
+        <option value="incomplete">Incompletos</option>
+        <option value="complete">Completos</option>
+      </Select>
+      <ToDoModal modalActive={modalActive} setModalActive={setModalActive} />
+    </ToDoHeaderStyle>
+  );
+};
+
+export default ToDoHeader;
