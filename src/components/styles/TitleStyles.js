8441583771895aefcb41b86c97f025f0649373b4
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-25 11:22:36 -0300

    Create Title styled component

---
+++ src/components/styles/TitleStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const TitleStyle = styled.h1`
+  display: inline-block;
+  width: 100%;
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 2rem;
+  margin-bottom: 1.5rem;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
