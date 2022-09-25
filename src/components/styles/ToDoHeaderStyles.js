300320ae875b8f7cc3e37754ed9b9f5a207a8734
Tiago Leite <tiagoleite1405@gmail.com>
2022-09-25 11:22:59 -0300

    Create ToDo header styled component

---
+++ src/components/styles/ToDoHeaderStyles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ToDoHeaderStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 60px;
+  margin-bottom: 2rem;
+`;
