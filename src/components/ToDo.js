b203fb12a242c7bc074512963eaf924b53eaf5aa
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-10 16:41:34 -0300

    Add and sort ToDo list items

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,22 @@
+import { useSelector } from 'react-redux';
 import { ToDoStyle } from './styles/ToDoStyles';
+import ToDoItem from './ToDoItem';
 
 const ToDo = () => {
-  return <ToDoStyle>ToDo</ToDoStyle>;
+  const todoList = useSelector((state) => state.todo.todoList);
+  const sortedTodoList = [...todoList].sort(
+    (a, b) => new Date(b.time) - new Date(a.time)
+  );
+
+  return (
+    <ToDoStyle>
+      <div>
+        {sortedTodoList && sortedTodoList.length > 0
+          ? sortedTodoList.map((task) => <ToDoItem todo={task} key={task.id} />)
+          : 'Ainda não tem nenhuma tarefa na lista'}
+      </div>
+    </ToDoStyle>
+  );
 };
 
 export default ToDo;
