29104e1d1650498efa2047aab3dbfc339dd97eb4
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-16 20:12:29 -0300

    Add checkbox functionality to update item status

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,15 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ToDoItemStyle } from './styles/ToDoItemStyles';
 import format from 'date-fns/format';
 import { MdDelete, MdEdit } from 'react-icons/md';
 import { useDispatch } from 'react-redux';
-import { deleteTodo } from '../redux/todoSlice';
+import { deleteTodo, updateTodo } from '../redux/todoSlice';
 import { toast } from 'react-hot-toast';
 import TodoModal from './ToDoModal';
+import CheckBox from './CheckBox';
 
 const ToDoItem = ({ todo }) => {
   const dispatch = useDispatch();
+
+  const [checked, setChecked] = useState(false);
+
   const [updateModalOpen, setupdateModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (todo.status === 'complete') {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [todo.status]);
 
   const deleteTask = () => {
     dispatch(deleteTodo(todo.id));
@@ -20,11 +32,18 @@ const ToDoItem = ({ todo }) => {
     setupdateModalOpen(true);
   };
 
+  const handleCheck = () => {
+    setChecked(!checked);
+    dispatch(
+      updateTodo({ ...todo, status: checked ? 'incomplete' : 'complete' })
+    );
+  };
+
   return (
     <>
       <ToDoItemStyle>
         <div className="todoDetails">
-          [ ]
+          <CheckBox checked={checked} handleCheck={handleCheck} />
           <div className="texts">
             <p
               className={`todoText ${
