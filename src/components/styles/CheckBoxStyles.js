95b98b1e385daa382459e76731f6a010e1afec8f
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-16 20:10:52 -0300

    Create CheckBox style component

---
+++ src/components/styles/CheckBoxStyles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const CheckBoxStyle = styled.div`
+  .svgBox {
+    flex-basis: 25px;
+    flex-shrink: 0;
+    height: 30px;
+    width: 30px;
+    border-radius: 2px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 6px;
+    cursor: pointer;
+    transition: 0.3s ease background-color;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.bg4};
+    }
+
+    svg {
+      width: 100%;
+      height: 100%;
+      stroke: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`;
