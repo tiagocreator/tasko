57b5719555d1aa55be0b6eca631164a2643295af
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-16 20:11:21 -0300

    Fix ToDo style component

--- src/components/styles/ToDoStyles.js
+++ src/components/styles/ToDoStyles.js
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
 export const ToDoStyle = styled.main`
-  .todo-wrapper {
-    max-width: 750px;
-    width: 100%;
-    margin: 0 auto;
-  }
+  width: 100%;
+  margin: 0 auto;
 
   .content-wrapper {
     background: ${({ theme }) => theme.bg2};
@@ -15,17 +12,5 @@ export const ToDoStyle = styled.main`
     @media (min-width: 768px) {
       font-size: 1.5rem;
     }
-  }
-  .empty-text {
-    font-size: 1.6rem;
-    font-weight: 500;
-    color: ${({ theme }) => theme.bg2};
-    text-align: center;
-    margin: 0 auto;
-    padding: 0.5rem 1rem;
-    border-radius: 8px;
-    background: ${({ theme }) => theme.bg4};
-    width: max-content;
-    height: auto;
   }
 `;
