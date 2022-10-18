9ba11b1035c66ba72aaf6eab7c9dcd0d3192b2f0
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-17 23:15:15 -0300

    Add filter todo items functionality

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,16 +3,24 @@ import { ToDoStyle } from './styles/ToDoStyles';
 import ToDoItem from './ToDoItem';
 
 const ToDo = () => {
+  const filterStatus = useSelector((state) => state.todo.filterStatus);
   const todoList = useSelector((state) => state.todo.todoList);
   const sortedTodoList = [...todoList].sort(
     (a, b) => new Date(b.time) - new Date(a.time)
   );
 
+  const filteredToDoList = sortedTodoList.filter((item) => {
+    if (filterStatus === 'all') return true;
+    return item.status === filterStatus;
+  });
+
   return (
     <ToDoStyle>
       <div className="content-wrapper">
-        {sortedTodoList && sortedTodoList.length > 0
-          ? sortedTodoList.map((task) => <ToDoItem todo={task} key={task.id} />)
+        {filteredToDoList && filteredToDoList.length > 0
+          ? filteredToDoList.map((task) => (
+              <ToDoItem todo={task} key={task.id} />
+            ))
           : 'Ainda não tem nenhuma tarefa na lista'}
       </div>
     </ToDoStyle>
