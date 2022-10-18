a384e9961aa85a7c4de5d08d5a3688ad4753a570
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-17 23:14:25 -0300

    Add onChange and value props to Select button

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,11 @@
 import { SelectStyle } from './styles/SelectStyles';
 
-const Select = ({ children }) => {
-  return <SelectStyle>{children}</SelectStyle>;
+const Select = ({ children, onChange, value }) => {
+  return (
+    <SelectStyle value={value} onChange={onChange}>
+      {children}
+    </SelectStyle>
+  );
 };
 
 export default Select;
