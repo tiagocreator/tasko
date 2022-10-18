d49e568bcc3a7dffea7095aea80652c3bc07c502
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-17 23:15:50 -0300

    Add filter todo items functionality

--- src/components/ToDoHeader.js
+++ src/components/ToDoHeader.js
@@ -1,22 +1,30 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { SecondaryButton } from './Button';
 import Select from './Select';
 import ToDoModal from './ToDoModal';
-
 import { ToDoHeaderStyle } from './styles/ToDoHeaderStyles';
+import { filterItems } from '../redux/todoSlice';
 
 const ToDoHeader = () => {
   const [modalActive, setModalActive] = useState(false);
+  const filterStatus = useSelector((state) => state.todo.filterStatus);
+
+  const dispatch = useDispatch();
 
   const openModal = () => {
     setModalActive(true);
   };
 
+  const updateFilterStatus = (e) => {
+    dispatch(filterItems(e.target.value));
+  };
+
   return (
     <ToDoHeaderStyle>
       <SecondaryButton onClick={openModal} text="Nova Tarefa" />
-      <Select>
+      <Select value={filterStatus} onChange={updateFilterStatus}>
         <option value="all">Todos</option>
         <option value="incomplete">Incompletos</option>
         <option value="complete">Completos</option>
