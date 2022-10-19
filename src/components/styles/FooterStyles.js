bf69214ff834665183f6a352d9771eec121753b1
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-19 11:15:25 -0300

    Create Footer styled component

---
+++ src/components/styles/FooterStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const FooterStyle = styled.footer`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70px;
+  background: transparent;
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: 1.3rem;
+    letter-spacing: 1px;
+  }
+
+  .github-icon {
+    margin-right: 5px;
+  }
+`;
