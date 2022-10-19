21d2db8f98ed8600c4fd7b65632be9618a79769d
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-19 11:15:05 -0300

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import { FooterStyle } from './styles/FooterStyles';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <FooterStyle>
+      <p>
+        <a
+          href="https://github.com/tiagocreator"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FaGithub className="github-icon" />
+          tiagocreator
+        </a>
+      </p>
+    </FooterStyle>
+  );
+};
+
+export default Footer;
