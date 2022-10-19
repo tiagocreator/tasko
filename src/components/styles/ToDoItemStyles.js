cff9a393a3da33f43cb2e4196aeb399bed626818
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-19 13:22:17 -0300

    Add framer motion animations to ToDo items

--- src/components/styles/ToDoItemStyles.js
+++ src/components/styles/ToDoItemStyles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const ToDoItemStyle = styled.div`
+export const ToDoItemStyle = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
