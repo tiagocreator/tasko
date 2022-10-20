610d1793e3a39ad3209b072c16966c8c8f00f8bf
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-20 20:51:47 -0300

    Refactor ToDoContainer as a page

--- src/pages/ToDoContainer.js
+++ src/pages/ToDoContainer.js
@@ -1,9 +1,11 @@
 import ToDoHeader from '../components/ToDoHeader';
 import ToDo from '../components/ToDo';
+import ButtonsContainer from '../components/ButtonsContainer';
 
 const ToDoContainer = () => {
   return (
     <>
+      <ButtonsContainer />
       <ToDoHeader />
       <ToDo />
     </>
