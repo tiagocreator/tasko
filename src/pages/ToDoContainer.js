d07161385bba975874a283c9ecde885524960eac
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-20 16:59:19 -0300

    Remove Title component

---
+++ src/pages/ToDoContainer.js
@@ -0,0 +1,13 @@
+import ToDoHeader from '../components/ToDoHeader';
+import ToDo from '../components/ToDo';
+
+const ToDoContainer = () => {
+  return (
+    <>
+      <ToDoHeader />
+      <ToDo />
+    </>
+  );
+};
+
+export default ToDoContainer;
