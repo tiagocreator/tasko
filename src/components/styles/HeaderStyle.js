6dbe573453066c7b185ee6d232e480a6fb2da3d4
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-24 00:10:05 -0300

    Remove burger menu

--- src/components/styles/HeaderStyle.js
+++ src/components/styles/HeaderStyle.js
@@ -21,31 +21,4 @@ export const HeaderStyle = styled.header`
   .logo img {
     margin-inline-end: 10px;
   }
-
-  .burger-menu {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .open-menu {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 2.8rem;
-  }
-
-  .close-menu {
-    padding: 3px;
-    width: 46px;
-    height: 46px;
-    border: 4px solid ${({ theme }) => theme.themeRed};
-    border-radius: 50%;
-  }
-
-  @media (min-width: 768px) {
-    .burger-menu {
-      display: none;
-    }
-  }
 `;
