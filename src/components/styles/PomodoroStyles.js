13ee5b0a443d754d09df0025441e60c0ceccfb23
Tiago Leite <tiagoleite1405@gmail.com>
2022-10-23 22:25:18 -0300

    Add Pomodoro styled component

---
+++ src/components/styles/PomodoroStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const PomodoroStyle = styled.div`
+  padding-block-start: 50px;
+  max-width: 320px;
+  margin: 0 auto;
+  text-align: center;
+
+  .buttons-container {
+    margin-block-start: 20px;
+  }
+`;
