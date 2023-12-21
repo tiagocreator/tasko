a810dcc2a4ea60459fb9f770cc42e48903da3b72
Tiago Leite <contato@tiagocreator.com>
2023-12-21 09:45:02 -0300

    Change props interface name

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,12 +1,12 @@
 import { SelectStyle } from './styles/SelectStyles';
 
-interface Props {
+interface SelectProps {
   children: React.ReactNode;
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   value: string;
 }
 
-const Select: React.FC<Props> = ({ children, onChange, value }) => {
+const Select: React.FC<SelectProps> = ({ children, onChange, value }) => {
   return (
     <SelectStyle value={value} onChange={onChange}>
       {children}
