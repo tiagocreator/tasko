e3a2e6e0f6bf4e48f7b30b87254c5798972f15f7
Tiago Leite <contato@tiagocreator.com>
2023-12-21 09:37:06 -0300

    Change routes logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import { ToDoPage, PomodoroPage } from './pages';
+import { Header, Footer } from './components';
+
 import { GlobalStyles } from './components/styles/GlobalStyles';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import { ThemeProvider } from 'styled-components';
 import { theme, MyTheme } from './themes';
-import { Toaster } from 'react-hot-toast';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import ToDoContainer from './pages/ToDoContainer';
-import PomodoroContainer from './pages/PomodoroContainer';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme as MyTheme}>
       <GlobalStyles />
-      <Header />
       <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path='/' element={<ToDoContainer />}></Route>
-          <Route path='/pomodoro' element={<PomodoroContainer />}></Route>
+          <Route path='/' element={<ToDoPage />}></Route>
+          <Route path='/pomodoro' element={<PomodoroPage />}></Route>
         </Routes>
       </BrowserRouter>
       <Footer />
