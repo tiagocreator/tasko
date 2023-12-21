11a18c70e086a21d57cd9a2738d117d77b6c5dc8
Tiago Leite <contato@tiagocreator.com>
2023-12-21 09:43:08 -0300

    Move github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
-import { FooterStyle } from './styles/FooterStyles';
 import { FaGithub } from 'react-icons/fa';
 
+import { FooterStyle } from './styles/FooterStyles';
+
 const Footer: React.FC = () => {
+  const githubLink: string = 'https://github.com/tiagocreator';
+
   return (
     <FooterStyle>
-      <p>
-        <a href='https://github.com/tiagocreator' rel='noreferrer' target='_blank'>
-          <FaGithub className='github-icon' />
-          tiagocreator
-        </a>
-      </p>
+      <a href={githubLink} rel='noreferrer' target='_blank'>
+        <FaGithub className='github-icon' />
+        tiagocreator
+      </a>
     </FooterStyle>
   );
 };
